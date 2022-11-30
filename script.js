70b3b3af28755cc889eb6c9716ebe9e490ed2b04
thilger1 <79420353+thilger1@users.noreply.github.com>
2022-11-30 08:37:14 -0500

    Update score after each game

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
+const player = document.getElementById('playerChoice');
+const computer = document.getElementById('computerChoice');
 
-const playerScore = 0;
-const computerScore = 0;
-const showPlayerScore = document.getElementById('playerScore');
-const showComputerScore = document.getElementById('computerScore');
+const gameInfo = document.getElementById("game-info");
+const btn = document.querySelectorAll('button');
+let winCount = 0;
+let loseCount = 0;
+
+//displays player choice and random computer choice and plays round
+btn.forEach((button) =>{
+    button.addEventListener('click', function (e) {
+        let playerChoice = button.id
+        if (playerChoice == "rock"){
+            player.src="images/rock.png"
+        }
+        if (playerChoice == "paper"){
+            player.src="images/paper.png"
+        }
+        if (playerChoice == "scissors"){
+            player.src="images/scissors.png"
+        }
+        let computerChoice = getComputerChoice();
+        if (computerChoice == "rock"){
+            computer.src="images/rock.png"
+        }
+        if (computerChoice == "paper"){
+            computer.src="images/paper.png"
+        }
+        if (computerChoice == "scissors"){
+            computer.src="images/scissors.png"
+        }
+
+        round(playerChoice, computerChoice);
+    });
+});
 
 function getComputerChoice(){
     let answer = Math.floor(Math.random() * 3);
@@ -18,61 +48,19 @@ function getComputerChoice(){
     return answer
 }
 
-function getPlayerChoice(){
-    // prompting player for choice and enabling input exceptions
-    let playerChoice = prompt("What do you choose? (Rock/Paper/Scissors)");
-    playerChoice = playerChoice.toLowerCase();
-    if (playerChoice.includes('rock')){
-        playerChoice = "Rock";
-    }
-    else if (playerChoice.includes('paper')){
-        playerChoice = "Paper";
-    }
-    else if (playerChoice.includes('scissors')){
-        playerChoice = "Scissors";
-    }
-    else {
-        alert("Sorry, I did not understand your entry");
-        return getPlayerChoice();
-    }
-
-    return playerChoice;
-
-}
-
-function win(player, computer){
-    let playerChoice = player;
-    let computerChoice = computer;
-    let gameInfo = document.getElementById("game-info");
-
-    gameInfo.textContent = "You win! " + playerChoice + " beats " + computerChoice;
-    return score(1);
-}
-
-function lose(player, computer){
-    let playerChoice = player;
-    let computerChoice = computer;
-    let gameInfo = document.getElementById("game-info");
-
-    gameInfo.textContent = "You lost! " + computerChoice + " beats " + playerChoice;
-    return score(0);
-}
-
 function round(player, computer){
 
-    
     let computerChoice = computer;
     let playerChoice = player;
-    let gameInfo = document.getElementById("game-info");
-
     
     if (computerChoice == playerChoice){
         gameInfo.textContent = playerChoice + " ties " + playerChoice + "!";
+        return;
     }
     // Computer Rock
     else if(computerChoice == "rock"){
         if (playerChoice == "paper"){
-            win(playerChoice, computerChoice);
+            return win(playerChoice, computerChoice);
         }
         else{
             return lose(playerChoice, computerChoice);
@@ -98,97 +86,59 @@ function round(player, computer){
     }
 }
 
-function playRound(games){
-    let winCount = 0;
-    let loseCount = 0;
-    while (winCount < games && loseCount < games){
-        let didWin = round()
-        if (didWin == 1){
-            winCount += 1;
-        }
-        else if (didWin == 0){
-            loseCount += 1; 
-        }
-        alert("You: " + winCount + "\nComputer: " + loseCount)
-    }
-    if (winCount == games){
-        alert("You won the game! Congratulations!")
-    }
-    else{
-        alert("The computer won! Play again!")
+function win(player, computer){
+    let playerChoice = player;
+    let computerChoice = computer;
 
-    }
+    gameInfo.textContent = "You win! " + playerChoice + " beats " + computerChoice;
+    return score(1);
+}
 
+function lose(player, computer){
+    let playerChoice = player;
+    let computerChoice = computer;
+    let gameInfo = document.getElementById("game-info");
+
+    gameInfo.textContent = "You lost! " + computerChoice + " beats " + playerChoice;
+    return score(0);
 }
 
 
+function score(result) {
 
-function score(result){
-    if (playerScore == 5){
+    let playerScore = document.getElementById('playerScore');
+    let computerScore = document.getElementById('computerScore');
+
+    if (result == 1){
+        winCount += 1;
+        playerScore.textContent = `You: ${winCount}`;
+    }
+    if (result == 0){
+        loseCount += 1;
+        computerScore.textContent = `Computer: ${loseCount}`; 
+    }
+    if (winCount == 5){
         return reset(1);
     }
-    else if (computerScore == 5){
+    else if (loseCount == 5){
         return reset(0);
     }
     else{
-        if (result == 1){
-            playerScore += 1;
-            showPlayerScore.textContent = `You: ${playerScore}`;
-            }
-        if (result == 0){
-            computerScore += 1;
-            showComputerScore.textContent = `Computer: ${computerScore}`; 
-        }
+        return;
     }
-    
 }
 
 function reset(result){
     if (result == 1){
-        alert("Game over, you win!")
+        alert("Game over, you win!");
     }
     else{
-        alert("Game over, you lose!")
+        alert("Game over, you lose!");
     }
-    playerScore = 0;
-    computerScore = 0;
+    winCount = 0;
+    loseCount = 0;
     return;
 }
 
-function start(){
-    alert("Welcome to RPS! You will now play games against the computer")
-    playRound(5)
-}
 
-const btn = document.querySelectorAll('button');
-const player = document.getElementById('playerChoice');
-const computer = document.getElementById('computerChoice');
-
-btn.forEach((button) =>{
-    button.addEventListener('click', function (e) {
-        let playerChoice = button.id
-        if (playerChoice == "rock"){
-            player.src="images/rock.png"
-        }
-        if (playerChoice == "paper"){
-            player.src="images/paper.png"
-        }
-        if (playerChoice == "scissors"){
-            player.src="images/scissors.png"
-        }
-        let computerChoice = getComputerChoice();
-        if (computerChoice == "rock"){
-            computer.src="images/rock.png"
-        }
-        if (computerChoice == "paper"){
-            computer.src="images/paper.png"
-        }
-        if (computerChoice == "scissors"){
-            computer.src="images/scissors.png"
-        }
-
-        round(playerChoice, computerChoice);
-    
-    });
-});
 
