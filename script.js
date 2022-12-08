9f63f383f7ca70d6f731205c56322f79c4d627a2
thilger1 <79420353+thilger1@users.noreply.github.com>
2022-12-07 22:11:16 -0500

    Properly size footer, overlay with reset on win/loss

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const computer = document.getElementById('computerChoice');
 
 const gameInfo = document.getElementById("game-info");
 const gameMessage = document.getElementById('game-message');
-const btn = document.querySelectorAll('button');
+const endMessage = document.getElementById('endMessage');
+const btn = document.querySelectorAll('.btn');
+const reset = document.getElementById('reset');
 let winCount = 0;
 let loseCount = 0;
 
@@ -12,6 +14,8 @@ btn.forEach((button) =>{
     button.addEventListener('click', function (e) {
 
         let playerChoice = button.id
+        let computerChoice = getComputerChoice();
+
         if (playerChoice == "rock"){
             player.src="images/rock.png"
         }
@@ -21,7 +25,6 @@ btn.forEach((button) =>{
         if (playerChoice == "scissors"){
             player.src="images/scissors.png"
         }
-        let computerChoice = getComputerChoice();
         if (computerChoice == "rock"){
             computer.src="images/rock.png"
         }
@@ -56,10 +59,8 @@ function round(player, computer){
     let playerChoice = player;
     
     if (computerChoice == playerChoice){
-        console.log(playerChoice);
-        console.log(computerChoice);
         gameMessage.textContent = "Tie!";
-        gameInfo.textContent = `${capitalize(playerChoice)} ties ${playerChoice}!`;
+        gameInfo.textContent = `${capitalize(playerChoice)} ties ${playerChoice}`;
         return;
     }
     // Computer Rock
@@ -95,7 +96,7 @@ function win(player, computer){
     let playerChoice = player;
     let computerChoice = computer;
 
-    gameMessage.textContent = "You win!"
+    gameMessage.textContent = "You win the round!"
     gameInfo.textContent = `${capitalize(playerChoice)} beats ${computerChoice}`;
     return score(1);
 }
@@ -105,7 +106,7 @@ function lose(player, computer){
     let computerChoice = computer;
     let gameInfo = document.getElementById("game-info");
 
-    gameMessage.textContent = "You lose!"
+    gameMessage.textContent = "You lose the round!"
     gameInfo.textContent = `${capitalize(computerChoice)} beats ${playerChoice}`;
     return score(0);
 }
@@ -125,20 +126,20 @@ function score(result) {
         computerScore.textContent = `Computer: ${loseCount}`;
     }
     if (winCount == 5){
-        return reset(1);
+        return end(1);
     }
     if (loseCount == 5){
-        return reset(0);
+        return end(0);
     }
     return;
 }
 
-function reset(result){
+function end(result){
     if (result == 1){
-        alert("Game over, you win!");
+        endMessageOn();
     }
     else{
-        alert("Game over, you lose!");
+        endMessageOn();
     }
     winCount = 0;
     loseCount = 0;
@@ -150,4 +151,24 @@ function capitalize(word){
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
 }
 
+function endMessageOn(){
+    endMessage.style.display = 'block';
+    return;
+}
+
+function endMessageOff(){
+    endMessage.style.display = "none";
+    return;
+}
+
+reset.addEventListener('click', function() {
+    winCount = 0;
+    loseCount = 0;
+    playerScore.textContent = 'You: 0';
+    computerScore.textContent = 'Computer: 0';
+    gameInfo.textContent = "Play the computer, best of 5 games"
+    gameMessage.textContent = "Start by pressing any button";
+    endMessageOff();
+    return;
+});
 
